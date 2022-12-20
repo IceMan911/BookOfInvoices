fd239d7ce8b5aa586cb3e5b395be01ea88309335
David Novák <novak.david@cmsys.cz>
2022-12-20 22:09:16 +0100

    git init

---
+++ BookOfInvoices/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BookOfInvoices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
